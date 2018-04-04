df3b760d0cca6e3bfbdc16d33af1db2ae708e9f1
00017316 <00017316@uca.edu.sv>
2018-04-04 11:47:22 -0600

    Initial commit

---
+++ app/src/main/java/com/meag/lab03/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.meag.lab03;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent callingIntent = getIntent();
+        String intentAction = callingIntent.getAction();
+        String intentType = callingIntent.getType();
+        if (Intent.ACTION_SEND.equals(intentAction) && intentType != null){
+            if (intentType.equals("text/plain")){
+                handleReceivedText(callingIntent);
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/meag/lab03/MainActivity.java
@@ -0,0 +1,19 @@
+package com.meag.lab03;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void AbrirActivity2(View v){
+    Intent sndScreenIntent = new Intent(getApplicationContext(), Main2Activity.class);
+    startActivity(sndScreenIntent);}
+}
